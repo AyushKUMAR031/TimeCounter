7c4829953e2876672b71da693570a03c101ff332
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2024-04-18 01:53:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let targetTime;
+      let intervalId;
+
+      function startTimer() {
+        const hours = parseInt(document.getElementById("hours").value) || 0;
+        const minutes = parseInt(document.getElementById("minutes").value) || 0;
+        const seconds = parseInt(document.getElementById("seconds").value) || 0;
+
+        //calculating time
+        targetTime =
+          new Date().getTime() +
+          hours * 60 * 60 * 1000 +
+          minutes * 60 * 1000 +
+          seconds * 1000;
+
+        //counting
+        intervalId = setInterval(updateCountdown, 1000);
+      }
+
+      function updateCountdown() {
+        const currentDate = new Date().getTime();
+        const remainingTime = targetTime - currentDate;
+
+        // Check if the countdown has ended
+        if (remainingTime < 0) {
+          clearInterval(intervalId);
+          document.getElementById("countdown").innerHTML = "Countdown expired";
+        } else {
+          // Calculate hours, minutes, and seconds
+          const hours = Math.floor(remainingTime / (1000 * 60 * 60));
+          const minutes = Math.floor(
+            (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
+          );
+          const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+          // Display the countdown
+          document.getElementById(
+            "countdown"
+          ).innerHTML = `${hours}h ${minutes}m ${seconds}s`;
+        }
+      }
