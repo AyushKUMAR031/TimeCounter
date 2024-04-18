bbee70a20814d26accdcd91a9daeabe9266e1fc9
AyushKr.19 <120276832+AyushKUMAR031@users.noreply.github.com>
2024-04-18 12:06:28 +0530

    script.js

--- script.js
+++ script.js
@@ -1,43 +1,53 @@
 let targetTime;
-      let intervalId;
+let intervalId;
 
-      function startTimer() {
-        const hours = parseInt(document.getElementById("hours").value) || 0;
-        const minutes = parseInt(document.getElementById("minutes").value) || 0;
-        const seconds = parseInt(document.getElementById("seconds").value) || 0;
+function startTimer() {
+  const hours = parseInt(document.getElementById("hours").value) || 0;
+  const minutes = parseInt(document.getElementById("minutes").value) || 0;
+  const seconds = parseInt(document.getElementById("seconds").value) || 0;
 
-        //calculating time
-        targetTime =
-          new Date().getTime() +
-          hours * 60 * 60 * 1000 +
-          minutes * 60 * 1000 +
-          seconds * 1000;
+  //calculating time
+  targetTime =
+    new Date().getTime() +
+    hours * 60 * 60 * 1000 +
+    minutes * 60 * 1000 +
+    seconds * 1000;
 
-        //counting
-        intervalId = setInterval(updateCountdown, 1000);
-      }
+  //counting
+  intervalId = setInterval(updateCountdown, 1000);
+}
 
-      function updateCountdown() {
-        const currentDate = new Date().getTime();
-        const remainingTime = targetTime - currentDate;
+function stopTimer() {
+  clearInterval(intervalId);
+}
 
-        // Check if the countdown has ended
-        if (remainingTime < 0) {
-          clearInterval(intervalId);
-          document.getElementById("countdown").innerHTML = "Countdown expired";
-            let audio = new Audio("alertAudio.mp3");
-              audio.play();
-        } else {
-          // Calculate hours, minutes, and seconds
-          const hours = Math.floor(remainingTime / (1000 * 60 * 60));
-          const minutes = Math.floor(
-            (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
-          );
-          const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+function resetTimer() {
+  clearInterval(intervalId);
+  document.getElementById("countdown").innerHTML = "00h 00m 00s";
+}
 
-          // Display the countdown
-          document.getElementById(
-            "countdown"
-          ).innerHTML = `${hours}h ${minutes}m ${seconds}s`;
-        }
-      }
+function updateCountdown() {
+  const currentDate = new Date().getTime();
+  const remainingTime = targetTime - currentDate;
+
+  // Check if the countdown has ended
+  if (remainingTime < 0) {
+    clearInterval(intervalId);
+    document.getElementById("countdown").innerHTML = "Countdown expired";
+    let audio = new Audio("alertAudio.mp3");
+    audio.play();
+
+  } else {
+    // Calculate hours, minutes, and seconds
+    const hours = Math.floor(remainingTime / (1000 * 60 * 60));
+    const minutes = Math.floor(
+      (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+    // Display the countdown
+    document.getElementById(
+      "countdown"
+    ).innerHTML = `${hours}h ${minutes}m ${seconds}s`;
+  }
+}
